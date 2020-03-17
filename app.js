a5cac7a6b9ee63ea27c614fc46dacb0c9f6ad2fc
vladimirbasaric <vladimirbasaric@gmail.com>
2020-03-17 15:30:50 +0100

    modules (controllers) added

---
+++ app.js
@@ -0,0 +1,22 @@
+// Budget Controller
+let budgetController = (function(){
+
+    
+
+})();
+
+
+// UI Controller
+let UIController = (function(){
+
+    
+
+})();
+
+
+// Main Controller
+let controller = (function(budgetCtrl, IUCtrl) {
+
+
+
+})(budgetController, UIController);
